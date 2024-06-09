79c5b2b11cdd8f5910f27f5459ee3ed0987a296b
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-06-09 16:06:16 +0200

    [edit] dinamyc cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,68 @@ import Card from './components/Card'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const cities = [
+    {
+      id: 0,
+      name: "Tokyo",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, voluptas. ",
+      imgURL: "https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: true,
+    },
+    {
+      id: 1,
+      name: "New York",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, voluptas. ",
+      imgURL: "https://images.unsplash.com/photo-1485871981521-5b1fd3805eee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: false,
+    },
+    {
+      id: 2,
+      name: "Rome",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, voluptas. ",
+      imgURL: "https://images.unsplash.com/photo-1531572753322-ad063cecc140?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: true,
+    },
+    {
+      id: 3,
+      name: "Paris",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, voluptas. ",
+      imgURL: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: false,
+    },
+  ]
 
   return (
     <>
       <div className='grid grid-cols-4 gap-10'>
-        <Card 
-          title="Tokyo"
-          imgURL="https://images.unsplash.com/photo-1513407030348-c983a97b98d8?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum, culpa!
-          </Card>
-        <Card 
-          title="New York"
-          imgURL="https://images.unsplash.com/photo-1485871981521-5b1fd3805eee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
 
-          </Card>
-        <Card
-          title="Parigi"
-          imgURL="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+        {cities.map((city) => (
 
-          </Card>
-        <Card
-          title="Roma"
-          imgURL="https://images.unsplash.com/photo-1531572753322-ad063cecc140?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+          <Card 
+            key = {city.id}
+            title= {city.name}
+            isVisited= {city.isVisited}
+            imgURL=  {city.imgURL}>
+               {city.description}
+            </Card>
 
-          </Card>
+        ))}
+
+        {cities
+          .filter((city) => city.isVisited)
+          .map((city) => (
+
+          <Card 
+            key = {city.id}
+            title= {city.name}
+            isVisited= {city.isVisited}
+            imgURL=  {city.imgURL}>
+               {city.description}
+            </Card>
+
+        ))}
+
       </div>
 
       <div className="card">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-function Card ({title, imgURL, children}) {
+function Card ({title, imgURL, isVisited, children}) {
 
   return (
     <div className="rounded-md bg-zinc-950">
@@ -6,6 +6,11 @@ function Card ({title, imgURL, children}) {
       <div className="flex flex-col p-4">
         <h2 className="text-2x1 text-white font-bold">{ title }</h2>
         <p className="text-gray-500">{ children }</p>
+        {isVisited ? <span className="text-green-500">visitata</span> : <span className="text-red-500">non visitata</span>}
+        {/* {isVisited && <span className="text-green-500">visitata</span>}
+        {!isVisited && <span className="text-red-500">non visitata</span>} */}
+
+
       </div>
     </div>
     );
